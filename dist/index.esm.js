28438cc13393278bf58514a0b5f07d11c9fad9a0
dianasmara <dianasmara29@yahoo.com>
2022-01-24 11:12:36 +0700

    button

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -2852,8 +2852,8 @@ var templateObject_1$K;
 
 var baseColors = {
     failure: "#ED4B9E",
-    primary: "#aa8929",
-    primaryBright: "#aa8929",
+    primary: "#ED4B9E",
+    primaryBright: "#ED4B9E",
     primaryDark: "#aa8929",
     secondary: "#ED4B9E",
     success: "#31D0AA",
